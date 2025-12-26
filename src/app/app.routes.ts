d87e7dc0b44a3acaa0a384de6c3bfb40250f69aa
Reddy <2463713@cognizant.com>
2025-12-26 14:17:25 +0530

    adding css to admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './roles/auth/home/home.component';
 import { NgModule } from '@angular/core';
+import { AdminComponent } from './roles/admin/admin.component';
 
 export const routes: Routes = [
-    { path: '',component:HomeComponent}
+    { path: '',component:HomeComponent},
+    { path: 'admin',component:AdminComponent}
 ];
 
 @NgModule({
