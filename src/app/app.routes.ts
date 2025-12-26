51a1e2b8468bb39f5ebc9ab71d2206e081c7fbf0
M <2463684@cognizant.com>
2025-12-26 17:15:06 +0530

    Feat: Adding routes for my managerComponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './roles/auth/home/home.component';
 import { NgModule } from '@angular/core';
 import { SignupComponent } from './roles/auth/home/signup/signup.component';
+import { ManagerComponent } from './roles/manager/manager.component';
 
 export const routes: Routes = [
     { path: '',component:HomeComponent},
-    { path:'signup', component:SignupComponent}
+    { path:'signup', component:SignupComponent},
+    { path:'manager', component:ManagerComponent}
 ];
 
 @NgModule({
